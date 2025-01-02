046a2f24f018c5bb826d99cbba56d538aae109e9
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-01-02 12:06:30 +0100

    Dungeon Generation

---
+++ Assets/Scripts/Dungeon/AddRoom.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class AddRoom : MonoBehaviour {
+	void Start(){
+		RoomTemplates.instance.rooms.Add(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform Point_1;
+    public Transform Point_2;
+    
+    public float Speed = 5;
+    
+    
+    public float Idle_Time = 5;
+    int cap;
+    bool isPatroling = true;
+
+    private void Update()
+    {
+        if (isPatroling && cap == 1)
+        {
+            Patrol_Point_2();
+        }
+        if (isPatroling && cap == 0)
+        {
+            Patrol_Point_1();
+        }
+    }
+
+    private void Patrol_Point_1()
+    {
+        transform.localPosition = Vector2.MoveTowards(transform.position, Point_1.position, Speed * Time.deltaTime);        
+        if (transform.localPosition == Point_1.localPosition)
+        {
+            isPatroling = false;
+            StartCoroutine(Wait(1));
+        }
+    }
+    private void Patrol_Point_2()
+    {
+        transform.localPosition = Vector2.MoveTowards(transform.position, Point_2.position, Speed * Time.deltaTime);
+
+        if (transform.localPosition == Point_2.localPosition)
+        {
+            isPatroling = false;
+            StartCoroutine(Wait(0));
+        }
+    }
+    IEnumerator Wait(int index)
+    {
+        yield return new WaitForSeconds(Idle_Time);
+        isPatroling = true;
+        cap = index;        
+    }
+
+}

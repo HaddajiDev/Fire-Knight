0a59f57bb98243eaa95315e3f9622da2583d2461
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-01-04 17:23:01 +0100

    Room Objects

--- Assets/Scripts/Dungeon/RoomSize.cs
+++ Assets/Scripts/Dungeon/RoomSize.cs
@@ -8,9 +8,13 @@ public class RoomSize : MonoBehaviour
     public Transform Walls;
 
     public Transform SpawnPoints;
+
+    public GameObject Holder;
+    Room room;
     // Start is called before the first frame update
     void Start()
     {
+        
         if (SpawnPoints != null && SpawnPoints != null)
         {
             Walls.localScale = new Vector3(Size, Size, 1);
@@ -21,9 +25,64 @@ public class RoomSize : MonoBehaviour
             float currentSize = transform.localScale.x;
             transform.localScale = new Vector3(currentSize * Size, currentSize * Size, 1);
         }
+
+        if (Holder != null)
+        {
+            Holder.transform.localScale = new Vector3(Size, Size, 1);
+            for (int i = 0; i < Holder.transform.childCount; i++)
+            {
+                for (int j = 0; j < Holder.transform.GetChild(i).childCount; j++)
+                {
+                    Holder.transform.GetChild(i).transform.GetChild(j).transform.localScale = new Vector3(1 / Size, 1 / Size, 1);
+                }
+            }
+        }
+
+        Invoke(nameof(CheckForAll), 2f);
+
     }
 
-    // Update is called once per frame
+    private void CheckForAll()
+    {
+        if (TryGetComponent<Room>(out Room room))
+        {
+            if (room.roomType == Room.RoomType.Boss)
+            {
+                CheckForRoomType("Boss");
+            }
+            else if (room.roomType == Room.RoomType.traps)
+            {
+                CheckForRoomType("Traps");
+            }
+            else if (room.roomType == Room.RoomType.shop)
+            {
+                CheckForRoomType("Shop");
+            }
+            else if (room.roomType == Room.RoomType.Regular)
+            {
+                CheckForRoomType("Regular");
+            }
+            else if(room.roomType == Room.RoomType.Empty)
+            {
+                CheckForRoomType("Empty");
+            }
+        }
+    }
+
+    private void CheckForRoomType(string roomType)
+    {
+        for (int i = 0; i < Holder.transform.childCount; i++)
+        {
+            if (Holder.transform.GetChild(i).name == roomType)
+            {
+                Holder.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                Holder.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
     void Update()
     {
         

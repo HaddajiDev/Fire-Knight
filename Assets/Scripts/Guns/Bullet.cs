dda169f3569b6d92c207bf958f82795a5422a250
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-01-08 20:31:14 +0100

    fixing stuff

--- Assets/Scripts/Guns/Bullet.cs
+++ Assets/Scripts/Guns/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int Damage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        Destroy(gameObject);
     }
 }
